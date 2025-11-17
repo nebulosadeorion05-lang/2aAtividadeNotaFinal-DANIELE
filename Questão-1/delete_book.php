15600966079623dedaeeafcebcb3069ffeb59649
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-17 14:58:05 -0300

    Implement deleteBook function to remove a book

--- Questão-1/delete_book.php
+++ Questão-1/delete_book.php
@@ -1,1 +1,10 @@
+function deleteBook(id) {
 
+    fetch("http://localhost:3000/delete_book", {
+        method: "post"
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({id})
+    })
+    .then(res => res.json())
+    .then(() => location.reload());
+}
