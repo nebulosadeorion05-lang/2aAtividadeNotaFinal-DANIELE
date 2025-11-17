00c6724f43780081fb11aee451b77bded44b5acb
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-17 14:37:51 -0300

    Create database and books table in database.php

--- Questão-1/database.php
+++ Questão-1/database.php
@@ -1,7 +1,7 @@
-<?php
-$host = "localhost";
-$user = "";
-$pass = "";
-$dbnome = "Livraria";
-$
-?>
+CREATE DATABASE livraria;
+USE livraria;
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL
+);
