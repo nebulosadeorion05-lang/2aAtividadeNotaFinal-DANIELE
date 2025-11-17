87c8a4bc56f748716795e4ff3b52e0b1792a0102
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-17 14:57:10 -0300

    Implement addBook function to submit book data

--- Questão-1/add_book.php
+++ Questão-1/add_book.php
@@ -1,1 +1,12 @@
+function addBook() {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
 
+    fetch("http://localhost:3000/add_book", {
+        method: "post"
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({title, author})
+    })
+    .then(res => res.json())
+    .then(() => location.reload());
+}
