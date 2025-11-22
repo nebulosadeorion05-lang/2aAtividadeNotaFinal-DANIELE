742408090407a8e7e0003811a7d488da0250dbe8
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-22 11:38:59 -0300

    Update database.php to use PHP for database setup
    Refactor database creation and connection logic in PHP.

--- Questão-1/database.php
+++ Questão-1/database.php
@@ -1,7 +1,18 @@
-CREATE DATABASE livraria;
-CREATE TABLE IF NOT EXISTS livros (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    título TEXT NOT NULL,
-    autor TEXT NOT NULL,
-    ano INTEGER
-);
+<?php
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "livraria";
+
+$conexao = new mysql($host, $usuario, $senha, $banco);
+
+$sql = "CREATE TABLE IF NOT EXISTS livros (
+    ID INT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    data_publicacao DATE NOT NULL
+)";
+
+$conexao -> query($sql);
+$resultado = $conexao -> query("SELECT * FROM livros ORDER BY ID ASC");
+?>
