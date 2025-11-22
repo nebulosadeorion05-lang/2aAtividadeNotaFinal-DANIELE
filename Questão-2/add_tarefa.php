40b6eb482eb6e091470d7a57ef25b2e21027e4ac
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-22 16:54:23 -0300

    Fix variable name for due date in add_tarefa.php

--- Questão-2/add_tarefa.php
+++ Questão-2/add_tarefa.php
@@ -1,11 +1,11 @@
 <?php
 include 'database.php';
 
-if (isset($_POST["descricao"], $_POST["vencimento"])) {
+if (isset($_POST["descricao"], $_POST["data_vencimento"])) {
     $descricao = $_POST["descricao"];
     $data_vencimento = $_POST["data_vencimento"];
 
-    $sql = "INSERT INTO tarefas (descricao, vencimento) VALUES (?, ?)";
+    $sql = "INSERT INTO tarefas (descricao, data_vencimento) VALUES (?, ?)";
 
     $stmt = $conexao -> prepare($sql);
     $stmt -> bind_param("ss", $descricao, $data_vencimento);
