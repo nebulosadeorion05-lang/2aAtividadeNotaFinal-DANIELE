db13877aba700f0ccc0e351eea909606acdfd28f
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-22 15:36:43 -0300

    Implement delete functionality for tarefas

--- Questão-2/delete_tarefa.php
+++ Questão-2/delete_tarefa.php
@@ -1,1 +1,16 @@
+<?php
+include 'database.php';
 
+if (isset($_POST["id"])) {
+    $id = $_POST["id"];
+
+    $sql = "DELETE FROM tarefas WHERE id = ?";
+    $stmt = $conexao -> prepare($sql);
+    $stmt -> bind_param("i", $id);
+
+    if ($stmt -> execute()) {
+        header("Location: index.php");
+        exit;
+    } 
+}
+?>
