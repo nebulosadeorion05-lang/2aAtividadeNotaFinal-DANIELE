329f9c5b8d3c28709f4996a9c6a7eac703250097
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-22 15:29:49 -0300

    Implement task addition functionality

--- Questão-2/add_tarefa.php
+++ Questão-2/add_tarefa.php
@@ -1,1 +1,18 @@
+<?php
+include 'database.php';
 
+if (isset($_POST["descricao"], $_POST["vencimento"])) {
+    $descricao = $_POST["descricao"];
+    $data_vencimento = $_POST["data_vencimento"];
+
+    $sql = "INSERT INTO tarefas (descricao, vencimento) VALUES (?, ?)";
+
+    $stmt = $conexao -> prepare($sql);
+    $stmt -> bind_param("ss", $descricao, $data_vencimento);
+
+    if ($stmt -> execute()) {
+        header("Location: index.php");
+        exit;
+    } 
+}
+?>
