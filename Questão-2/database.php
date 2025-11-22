36aa702158814b8f03024adb92cd5f7e4348b296
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-22 15:24:59 -0300

    Create tarefas table in database.php

--- Questão-2/database.php
+++ Questão-2/database.php
@@ -1,1 +1,17 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "tarefas";
 
+$conexao = new mysqli($host, $usuario, $senha, $banco);
+
+$sql = "CREATE TABLE IF NOT EXISTS tarefas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao TEXT NOT NULL,
+    data_vencimento DATE NOT NULL,
+    concluida TINYINT(1) DEFAULT 0
+)";
+
+$conexao -> query($sql);
+?>
