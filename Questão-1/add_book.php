59b1eb60c399ec3e34db4dbf573a886110aa909e
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-24 13:54:44 -0300

    Update add_book.php

--- Questão-1/add_book.php
+++ Questão-1/add_book.php
@@ -3,19 +3,21 @@ require_once "database.php";
 
 if(isset($_POST["add_book"])) {
 
-    $titulo = $_POST["titulo"];
-    $autor = $_POST["autor"];
-    $ID = $_POST["ID"];
-    $data_publicacao = $_POST["data_publicacao"];
+    $titulo = $_POST["titulo"] ?? '';
+    $autor = $_POST["autor"] ?? '';
+    $ID = isset($_POST["ID"]) ? (int)$_POST["ID"] : null;
+    $data_publicacao = $_POST["data_publicacao"] ?? null;
 
-    $stmt = $conexao -> prepare("INSERT INTO livros (ID, titulo, autor, data_publicacao) VALUES (?, ?, ?, ?)");
-    $stmt -> bind_param("isss", $ID, $titulo, $autor, $data_publicacao);
-
-    if($stmt -> execute()) {
-        echo "<p> Cadastro feito com sucesso! </p>"
-    } else {
-        echo "<p> Erro ao efeturar cadastro. Tente novamente! </p>"
+    $stmt = $conexao->prepare("INSERT INTO livros (ID, titulo, autor, data_publicacao) VALUES (?, ?, ?, ?)");
+    if ($stmt === false) {
+        header("Location: index.php");
+        exit;
     }
+    $stmt->bind_param("isss", $ID, $titulo, $autor, $data_publicacao);
+
+    $stmt->execute();
+    $stmt->close();
+
     header("Location: index.php");
     exit;
 }
